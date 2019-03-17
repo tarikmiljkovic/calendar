beb8d7383ca76b007b8f3f1ad19674d13d047663
Tarik Miljković <tarik.miljkovic@stud.fh-campuswien.ac.at>
2019-03-17 15:04:06 +0100

    first

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Calendar App</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        
+       <nav>
+            <ul>
+                <li><a href="#">Calendar</a></li>
+                <li><a href="#">Groups</a></li>
+                <li><a href="#">Settings</a></li>
+            </ul>
+       </nav>
+
+
+
+
+
+    </body>
+</html>
