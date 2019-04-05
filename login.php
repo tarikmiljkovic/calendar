48a211b87c99930fa0f9e06883509a7940060708
mustafadag <mustafa.dag@stud.fh-campuswien.ac.at>
2019-04-05 22:30:46 +0200

    Revert "Revert "first""
    This reverts commit 99728b4933690e09ef290fae2620b27ad66658fe.

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Calendar App</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        
+        <h1>Calendar App</h1>
+        <input type="text" value="username">
+        <input type="password" value="">
+        <input type="submit" value="Login">
+
+        <?php
+
+        echo 2;
+    ?>
+
+    </body>
+</html>
